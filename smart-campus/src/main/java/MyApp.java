14b6de41666f84389cea460ecbfcb0b543a4f171
Sahansa Fernando <sahansaf2@gmail.com>
2026-04-15 16:05:40 +0530

    Setup
    Step 1 :The project & application configuration and the discovery Endpoints

---
+++ smart-campus/src/main/java/MyApp.java
@@ -0,0 +1,8 @@
+package com.mycompany.smart.campus;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api/v1")
+public class MyApp extends Application {
+}
